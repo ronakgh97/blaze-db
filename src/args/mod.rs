f1f882e144f0951b32f22f57115b546a94768695
Rounak Gh <ronakgh97@gmail.com>
2025-12-02 18:22:57 +0530

    Add new command list, new, server, ascii Modules

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -1,7 +1,12 @@
+mod ascii;
 mod create;
 mod init;
+mod list;
 mod new;
+mod serve;
 
-pub use create::create_run;
+pub use ascii::print_ascii;
 pub use init::init_run;
+pub use list::list_run;
 pub use new::new_run;
+pub use serve::serve_run;
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,6 +1,7 @@
 mod controller;
 mod dto;
 
+#[allow(unused_imports)]
 pub(crate) use dto::{CreateDatabaseRequest, CreateDatabaseResponse, HealthCheckResponse};
 
 pub use controller::start_server;
