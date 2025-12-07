500b044e1470a2a640185e2b8fa6acccc86a1794
Rounak Gh <ronakgh97@gmail.com>
2025-12-07 14:33:06 +0530

    Add read_embeddings_from_database function to retrieve embeddings from a specified database

--- src/server/service/embedder.rs
+++ src/server/service/embedder.rs
@@ -1,6 +1,6 @@
 use crate::server::service::database::search_database;
 use crate::server::{EmbedRequest, EmbedResponse};
-use crate::utils::{EmbeddingStore, Provider, log};
+use crate::utils::{EmbeddingStore, Provider, VectorData, log};
 use crate::{error, info};
 use anyhow::{Context, Result};
 
@@ -17,6 +17,7 @@ pub async fn embed_run(request: EmbedRequest) -> Result<EmbedResponse> {
         }
     };
 
+    //TODO: Provider should not be set on the fly
     // Configure embedding provider
     let url = "http://localhost:1234/v1/embeddings";
     let model = "text-embedding-qwen3-embedding-0.6b";
@@ -64,4 +65,19 @@ pub async fn embed_run(request: EmbedRequest) -> Result<EmbedResponse> {
         database: database_name,
         total_lines,
     })
+}
+
+/// Read embeddings from the specified database
+pub async fn read_embeddings_from_database(database: String) -> Result<VectorData> {
+    let database_path = match { search_database(database.clone()).await } {
+        Ok(path) => path,
+        Err(_e) => {
+            error!("Database '{}' not found", database);
+            return Err(_e).with_context(|| format!("Database '{}' not found", database));
+        }
+    };
+
+    let embedding_store = EmbeddingStore::read_binary(database_path.to_str().unwrap()).await?;
+
+    Ok(embedding_store)
 }
